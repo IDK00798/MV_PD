fe679272afdb8f199685ec114f4ca2ea26c0db76
thechosenone07 <86468050+thechosenone07@users.noreply.github.com>
2021-08-13 00:39:47 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -46,18 +46,18 @@ class Translation(object):
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-<b><a href="https://t.me/Mo_Tech_YT">MoTechYT</a></b>
+<b><a href="https://t.me/J_A_R_V_l_S">JARVIS</a></b>
 """
     
     ABOUT_TEXT = """<b>➥ 📃Name</b> : <b>Pro Auto Filter Bot</b>
     
-<b>➥ 😎Creator</b> : <b>@AlbertEinstein_TG</b> 
+<b>➥ 👽MAN</b> : <b>@J_A_R_V_l_S</b> 
 
-<b>➥ 👨‍💻Editor</b> : <b>@Mrk_YT</b>
+<b>➥ GROUP</b> : <b>@MV_PD</b>
 
 <b>➥ 🗣️Language</b> : <b>Python3</b>
 
 <b>➥ 📚Library</b> : <b><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></b>
 
-<b>➥ 📖Source Code</b> : <b><a href="https://github.com/MRK-YT/Pro-Auto-Filter-Bot">💥Click Me</a></b>
+<b>➥ 📖Source Code</b> : <b><a href="https://t.me/orthod_irunna_sankadam_varum/2">💥Click Me</a></b>
 """
